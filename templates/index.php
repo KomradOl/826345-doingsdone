2561c4671d8f5a1df0137b38ffb398f0a2cde99d
KomradOl <o.akinshin@gmail.com>
2019-02-07 15:20:28 +0400

    №7 Шаблонизация проекта

---
+++ templates/index.php
@@ -0,0 +1,45 @@
+<h2 class="content__main-heading">Список задач</h2>
+
+    <form class="search-form" action="index.php" method="post">
+        <input class="search-form__input" type="text" name="" value="" placeholder="Поиск по задачам">
+
+        <input class="search-form__submit" type="submit" name="" value="Искать">
+    </form>
+
+    <div class="tasks-controls">
+        <nav class="tasks-switch">
+            <a href="/" class="tasks-switch__item tasks-switch__item--active">Все задачи</a>
+            <a href="/" class="tasks-switch__item">Повестка дня</a>
+            <a href="/" class="tasks-switch__item">Завтра</a>
+            <a href="/" class="tasks-switch__item">Просроченные</a>
+        </nav>
+
+        <label class="checkbox">
+            <!--добавить сюда аттрибут "checked", если переменная $show_complete_tasks равна единице-->
+            <?php if($show_complete_tasks == 1) : ?>
+                <input class="checkbox__input visually-hidden show_completed" type="checkbox" checked>
+            <?php endif; ?>
+            <span class="checkbox__text">Показывать выполненные</span>
+        </label>
+    </div>
+
+    <table class="tasks">
+        <tr class="">
+            <th>Задачи</th>
+            <th>Дата выполнения</th>
+            <th>Категории</th>
+            <th>Выполнен</th>
+        </tr>
+        <?php foreach ($tasks_list as $task): ?> 
+            <? $completed = $task['completed'] ?>
+
+            <? if($show_complete_tasks != 0 || !$completed) : ?>
+                <tr class="tasks__item <?=$completed ? 'task--completed' : '' ?>">
+                    <td><?=$task['task']?></td>
+                    <td><?=$task['date']?></td>
+                    <td><?=$task['category']?></td>
+                    <td><?=$completed ? 'Да' : 'Нет' ?></td>
+                </tr>
+            <? endif;?>
+        <?php endforeach;?>
+    </table>
